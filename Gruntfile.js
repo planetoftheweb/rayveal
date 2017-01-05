ed43c3e7cabb00046cd73b79996c1fc19912c085
Ray Villalobos <ray@planetoftheweb.com>
2017-01-05 00:09:16 -0500

    reorganized files more to my liking q

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 /* global module:false */
 module.exports = function(grunt) {
 	var port = grunt.option('port') || 8000;
-	var base = grunt.option('base') || '.';
+	var base = grunt.option('base') || 'slideshow';
 
 	// Project configuration
 	grunt.initConfig({
@@ -17,33 +17,29 @@ module.exports = function(grunt) {
 				' */'
 		},
 
-		qunit: {
-			files: [ 'test/*.html' ]
-		},
-
 		uglify: {
 			options: {
 				banner: '<%= meta.banner %>\n'
 			},
 			build: {
-				src: 'js/reveal.js',
-				dest: 'js/reveal.min.js'
+				src: 'slideshow/js/reveal.js',
+				dest: 'slideshow/js/reveal.min.js'
 			}
 		},
 
 		sass: {
 			core: {
 				files: {
-					'css/reveal.css': 'css/reveal.scss',
+					'slideshow/css/reveal.css': 'process/scss/reveal.scss',
 				}
 			},
 			themes: {
 				files: [
 					{
 						expand: true,
-						cwd: 'css/theme/source',
+						cwd: 'process/scss',
 						src: ['*.scss'],
-						dest: 'css/theme',
+						dest: 'slideshow/css',
 						ext: '.css'
 					}
 				]
@@ -52,14 +48,14 @@ module.exports = function(grunt) {
 
 		autoprefixer: {
 			dist: {
-				src: 'css/reveal.css'
+				src: 'slideshow/css/reveal.css'
 			}
 		},
 
 		cssmin: {
 			compress: {
 				files: {
-					'css/reveal.min.css': [ 'css/reveal.css' ]
+					'slideshow/css/reveal.min.css': [ 'slideshow/css/reveal.css' ]
 				}
 			}
 		},
@@ -86,7 +82,7 @@ module.exports = function(grunt) {
 					exports: false
 				}
 			},
-			files: [ 'Gruntfile.js', 'js/reveal.js' ]
+			files: [ 'Gruntfile.js', 'slideshow/js/reveal.js' ]
 		},
 
 		connect: {
@@ -101,35 +97,29 @@ module.exports = function(grunt) {
 		},
 
 		zip: {
-			'reveal-js-presentation.zip': [
-				'index.html',
-				'css/**',
-				'js/**',
-				'lib/**',
-				'images/**',
-				'plugin/**',
-				'**.md'
+			'presentation.zip': [
+				'slideshow/**'
 			]
 		},
 
 		watch: {
 			js: {
-				files: [ 'Gruntfile.js', 'js/reveal.js' ],
+				files: [ 'Gruntfile.js', 'slideshow/js/reveal.js' ],
 				tasks: 'js'
 			},
 			theme: {
-				files: [ 'css/theme/source/*.scss', 'css/theme/template/*.scss' ],
+				files: [ 'process/scss/*.scss'],
 				tasks: 'css-themes'
 			},
 			css: {
-				files: [ 'css/reveal.scss' ],
+				files: [ 'process/scss/*.scss' ],
 				tasks: 'css-core'
 			},
 			html: {
-				files: [ '*.html']
+				files: [ 'slideshow/**/*.html']
 			},
 			markdown: {
-				files: [ '*.md' ]
+				files: [ 'slideshow/**/*.md' ]
 			},
 			options: {
 				livereload: true
@@ -139,7 +129,6 @@ module.exports = function(grunt) {
 	});
 
 	// Dependencies
-	grunt.loadNpmTasks( 'grunt-contrib-qunit' );
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
@@ -153,7 +142,7 @@ module.exports = function(grunt) {
 	grunt.registerTask( 'default', [ 'css', 'js' ] );
 
 	// JS task
-	grunt.registerTask( 'js', [ 'jshint', 'uglify', 'qunit' ] );
+	grunt.registerTask( 'js', [ 'jshint', 'uglify' ] );
 
 	// Theme CSS
 	grunt.registerTask( 'css-themes', [ 'sass:themes' ] );
@@ -171,6 +160,6 @@ module.exports = function(grunt) {
 	grunt.registerTask( 'serve', [ 'connect', 'watch' ] );
 
 	// Run tests
-	grunt.registerTask( 'test', [ 'jshint', 'qunit' ] );
+	grunt.registerTask( 'test', [ 'jshint' ] );
 
 };
