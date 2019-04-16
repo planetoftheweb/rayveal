11ae17a2deee7d7c9be0d78e06d7258f8f4e8de0
Ray Villalobos <ray@planetoftheweb.com>
2019-04-16 10:26:17 -0400

    changed gulp process in docs to use browsersync

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -1,56 +1,56 @@
-var gulp = require('gulp'),
-  webserver = require('gulp-server-io'),
-  folderIndex = require('gulp-folder-index'),
-  source = './',
-  dest = './';
+var gulp = require("gulp"),
+  folderIndex = require("gulp-folder-index"),
+  browserSync = require("browser-sync").create(),
+  source = "./",
+  dest = "./";
 
 function html() {
-  return gulp.src(dest + '**/*.html');
+  return gulp.src(dest + "**/*.html");
 }
 
 function js() {
-  return gulp.src(dest + '**/*.js');
+  return gulp.src(dest + "**/*.js");
 }
 
 function styles() {
-  return gulp.src(dest + '**/*.css');
+  return gulp.src(dest + "**/*.css");
 }
 
 function slides() {
   return gulp
-    .src(source + 'slides/**/*.md')
+    .src(source + "slides/**/*.md")
     .pipe(
       folderIndex({
-        extension: '.md',
-        filename: 'index.json'
+        extension: ".md",
+        filename: "index.json"
       })
     )
-    .pipe(gulp.dest(dest + 'slides'));
+    .pipe(gulp.dest(dest + "slides"));
 }
 
 function watch() {
-  gulp.watch(source + 'js/**/*.js', js);
-  gulp.watch(source + 'css/**/*.css', styles);
-  gulp.watch(source + 'index.html', html);
-  gulp.watch(source + 'slides/**/*.md', slides);
+  gulp.watch(source + "js/**/*.js", js).on("change", browserSync.reload);
+  gulp.watch(source + "css/**/*.css", styles).on("change", browserSync.reload);
+  gulp.watch(source + "index.html", html).on("change", browserSync.reload);
+  gulp
+    .watch(source + "slides/**/*.md", slides)
+    .on("change", browserSync.reload);
 }
 
 function server() {
-  return gulp.src(source).pipe(
-    webserver({
-      serverReload: {
-        dir: source
-      },
-      port: 3333,
-      open: true
-    })
-  );
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+
+  gulp.watch(source + "css/**/*.css", styles).on("change", browserSync.reload);
+  gulp.watch(source + "index.html", html).on("change", browserSync.reload);
+  gulp
+    .watch(source + "slides/**/*.md", slides)
+    .on("change", browserSync.reload);
 }
 
-var build = gulp.series(
-  gulp.parallel(slides, js, styles, html),
-  server,
-  watch
-);
+var build = gulp.series(gulp.parallel(slides, js, styles, html), server, watch);
 
-gulp.task('default', build);
+gulp.task("default", build);
